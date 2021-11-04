2d085a6430be8653d158e0e4dc4747de45143abb
Saurabh-Thakur-07 <saurabhthakurmeerut@gmail.com>
2021-11-05 02:32:52 +0530

    completed project 3

--- Rotating-navigation-animation/script.js
+++ Rotating-navigation-animation/script.js
@@ -1,1 +1,5 @@
-console.log('Hello world!!!');
+const openB = document.getElementById('open');
+const closeB = document.getElementById('close');
+const container = document.querySelector('.container');
+openB.addEventListener('click',()=> container.classList.add('show-nav'));
+closeB.addEventListener('click',()=> container.classList.remove('show-nav'));
