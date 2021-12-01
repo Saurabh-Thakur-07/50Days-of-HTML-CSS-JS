4d52c5af9cb608cec9aaf551e6e0e1bb15ec1696
Saurabh-Thakur-07 <saurabhthakurmeerut@gmail.com>
2021-12-01 07:30:08 +0530

    completed project 5

--- Blurry-loading/script.js
+++ Blurry-loading/script.js
@@ -1,1 +1,16 @@
-console.log('hello world!!');
+const loadText = document.querySelector('.loading-text');
+const background = document.querySelector('.bg');
+let int = setInterval(blurry,30);
+let load = 0 ;
+function blurry(){
+    load++;
+    if(load > 99){
+        clearInterval(int);
+    }
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load,0,100,1,0);
+    background.style.filter = `blur(${scale(load,0,100,30,0)}px)`;    
+}
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
